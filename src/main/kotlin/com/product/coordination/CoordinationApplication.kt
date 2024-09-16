c9dcc7960627f790957ace4846fb4f2fe9167fe1
donggyukim <joe.swe.0@gmail.com>
2024-09-16 15:28:46 +0900

    feat: project init

---
+++ src/main/kotlin/com/product/coordination/CoordinationApplication.kt
@@ -0,0 +1,11 @@
+package com.product.coordination
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoordinationApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoordinationApplication>(*args)
+}
