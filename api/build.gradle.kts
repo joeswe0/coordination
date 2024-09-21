472817ef59c3918c3cbddcd8eda456ce624b9a93
donggyukim <joe.swe.0@gmail.com>
2024-09-21 17:34:04 +0900

    docs: swagger setting

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,6 +7,8 @@ tasks.getByName("jar") {
 }
 
 dependencies {
+    implementation(project(":core"))
     implementation(project(":persistence:h2"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 }
---
+++ api/src/main/kotlin/com/commerce/coordination/SwaggerConfig.kt
@@ -0,0 +1,20 @@
+package com.commerce.coordination
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components())
+        .info(apiInfo())
+
+    private fun apiInfo() = Info()
+        .title("코디 서비스")
+        .version("1.0.0")
+}
